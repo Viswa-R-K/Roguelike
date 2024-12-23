603f490d39ad26f426113c8111c3a517c290b3bb
Viswa R K <rkviswakrishnan@gmail.com>
2024-12-23 20:06:24 +0530

    improved : smooth movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,19 @@ public class PlayerController : MonoBehaviour
     private BoardManager m_Board;
     private Vector2Int m_CellPosition;
     private bool m_IsGameOver;
+    public float MoveSpeed = .05f;
+    private bool m_IsMoving;
+    private Vector3 m_MoveTarget;
+
 
     public void Spawn(BoardManager boardManager, Vector2Int cell){
         m_Board = boardManager;
         m_CellPosition = cell;
 
-        transform.position = m_Board.CellToWorld(cell);
+        MoveTo(m_CellPosition,true);
     }
     public void Init(){
+        m_IsMoving = false;
         m_IsGameOver = false;
     }
 
@@ -21,9 +26,16 @@ public class PlayerController : MonoBehaviour
         m_IsGameOver = true;
     }
 
-    public void MoveTo(Vector2Int cell){
+    public void MoveTo(Vector2Int cell, bool immediate){
         m_CellPosition = cell;
-        transform.position = m_Board.CellToWorld(cell);
+        if(!immediate){
+            m_IsMoving = true;
+            m_MoveTarget = m_Board.CellToWorld(cell);
+        }
+        else{
+            m_IsMoving = false;
+            transform.position = m_Board.CellToWorld(cell);
+        }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -37,6 +49,17 @@ public class PlayerController : MonoBehaviour
         if(m_IsGameOver){
             if(Keyboard.current.enterKey.wasPressedThisFrame){
                 GameManager.Instance.StartNewGame();
+            }
+            return;
+        }
+        if(m_IsMoving){
+            transform.position = Vector3.MoveTowards(transform.position,m_MoveTarget,MoveSpeed);
+            if(transform.position == m_MoveTarget){
+                m_IsMoving = false;
+                var cellData = m_Board.GetCellData(m_CellPosition);
+                if(cellData.containedObject != null){
+                    cellData.containedObject.PlayerEntered();
+                }
             }
             return;
         }
@@ -63,11 +86,10 @@ public class PlayerController : MonoBehaviour
             if(cellData != null && cellData.passable){
                 GameManager.Instance.turnManager.Tick();
                 if(cellData.containedObject == null){
-                    MoveTo(newCellTarget);
+                    MoveTo(newCellTarget,false);
                 }
                 else if(cellData.containedObject.PlayerWantsToEnter()){
-                    MoveTo(newCellTarget);
-                    cellData.containedObject.PlayerEntered();
+                    MoveTo(newCellTarget,false);
                 }
             } 
         }
